b8da32b1fd91314452476ed4c80d4d4dee5666cc
Abhishek Patel <abhishelpatel171019@gmail.com>
2023-10-06 19:53:27 +0530

    Changed folder structure to add frontend

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index.js');
-
-const ThoughtService = require('./services/thoughtService');
-const thoughtService = new ThoughtService();
 
 const { setUpJob } = require('./services/scheduleService')
 
 const app = express();
 
+const static_path = path.join(__dirname, '../public');
+
 function setUpAndStartServer(){
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use(express.static(static_path));
 
     app.get('/', (req, res) => {
         return res.status(200).send('Server is ready');
